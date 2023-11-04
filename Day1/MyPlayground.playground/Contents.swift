0164a01695cdc8d28acb0a06c3121a78ea88721c
devnizamuddin <dev.nizamuddin@gmail.com>
2023-11-05 04:39:19 +0600

    String

--- Day1/MyPlayground.playground/Contents.swift
+++ Day1/MyPlayground.playground/Contents.swift
@@ -11,3 +11,17 @@ let name = "Nizam Uddin Shamrat"
 //name="Not Possilbe" //Not Possilbe int constant
 print(name)
 
+//------String-------//
+let resut = "We win the game 😎"
+let lineBreak = "We\nwin the game 😎"
+let quotedText = "The he teaches us how to \"Belive\" him"
+let multiLine = """
+nizam uddin
+Shamrat
+"""
+print(multiLine)
+
+//String Functions
+print(name.count)
+print(resut.hasPrefix("We"))
+print(quotedText.hasSuffix("him"))
