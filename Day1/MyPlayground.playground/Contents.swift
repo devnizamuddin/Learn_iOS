a8626c666fa1a0486f4dbc7f83b7f8f5b93fa813
devnizamuddin <dev.nizamuddin@gmail.com>
2023-11-05 04:57:37 +0600

    Number

--- Day1/MyPlayground.playground/Contents.swift
+++ Day1/MyPlayground.playground/Contents.swift
@@ -1,17 +1,21 @@
 import Cocoa
 
-//------Variable-------//
+/* This file created At 5th Nov 2023
+ * Auther: (devnizamuddin) Nizam Uddin Shamrat
+ */
+
+// ------Variable-------//
 var greeting = "Hello, playground"
 greeting = "Playground"
 greeting = "Hello"
 print(greeting)
 
-//------Constant-------/
+// ------Constant-------/
 let name = "Nizam Uddin Shamrat"
-//name="Not Possilbe" //Not Possilbe int constant
+// name="Not Possilbe" //Not Possilbe int constant
 print(name)
 
-//------String-------//
+// ------String-------//
 let resut = "We win the game 😎"
 let lineBreak = "We\nwin the game 😎"
 let quotedText = "The he teaches us how to \"Belive\" him"
@@ -20,8 +24,22 @@ nizam uddin
 Shamrat
 """
 print(multiLine)
-
-//String Functions
+// String Functions
 print(name.count)
 print(resut.hasPrefix("We"))
 print(quotedText.hasSuffix("him"))
+
+// ------Number-------//
+let score = 10
+let longNumber = 900_000_000_000 // _ to separate Long number(Veiw Only, not in print)
+print(longNumber)
+
+let lowerScroe = score - 2
+
+var count = 5
+count = count + 5 // short hand assignment operator
+count += 5 // Compound Assignment Operator
+
+print(count)
+print(count.isMultiple(of: 3))
+print(16.isMultiple(of: 3))
